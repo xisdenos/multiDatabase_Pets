b830cab607bb982180ae76032162198e82cb84f2
razante <lucaspinto.andrade@gmail.com>
2022-01-19 16:19:57 -0300

    test: create one pet in postgres

--- src/tests/postgres.test.js
+++ src/tests/postgres.test.js
@@ -4,11 +4,26 @@ import { ContextStrategy } from "../strategies/base/contextStrategy.js";
 
 
 const context = new ContextStrategy(new PostgresDB())
+const MOCK_PET_CADASTRAR = {
+    nome: 'lizeu',
+    raca: 'kuvasz'
+}
 
 describe('Postgres Strategy', function ()  {
     this.timeout(Infinity)
+    this.beforeAll(async function() {
+        await context.connect()
+    })
+    
+    
     it('PostgresSQL connection', async () => {
         const result = await context.isConnected()
         assert.equal(result, true)
+    })
+
+    it('cadastrar', async () => {
+        const result = await context.create(MOCK_PET_CADASTRAR)
+        delete result.id
+        assert.deepEqual(result, MOCK_PET_CADASTRAR)
     })
 })
